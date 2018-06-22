1abe00e8f18b5dc15013b564920e8a70ff6c3215
AlbertoCortina <albertocortina96@gmail.com>
2018-06-22 22:14:40 +0200

    Estructura de proyecto v1

---
+++ Hack4SD/Configuration/Factory.cs
@@ -0,0 +1,19 @@
+﻿using Business.Services;
+using Business.Services.Impl;
+using Persistence.Implementations;
+using Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Factory
+    {
+        public static IService getService() => new IServiceImpl();
+
+        public static IPersistence getPersistence() => new IPersistenceImpl();
+    }
+}
--- Hack4SD/Web/About.aspx.cs
+++ Hack4SD/Web/About.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,7 @@ namespace Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+           
         }
     }
 }
