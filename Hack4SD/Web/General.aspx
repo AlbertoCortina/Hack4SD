8fd5414d167c45c4e24a3e6a33eecd703a571984
Lamasape <32495756+Lamasape@users.noreply.github.com>
2018-06-24 00:10:33 +0200

    GeneralExperiencia

---
+++ Hack4SD/Web/General.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="General.aspx.cs" Inherits="Web.General" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+  
+    <%//Fin div contenedor %>
+</asp:Content>
--- Hack4SD/Web/General.aspx.cs
+++ Hack4SD/Web/General.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +8,11 @@ using System.Web.UI.WebControls;
 
 namespace Web
 {
-    public partial class _Default : Page
+    public partial class General : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+           
         }
     }
 }
