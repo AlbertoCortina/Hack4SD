34832f7155498ef5aa1f6e02c281fa90c2bf67f1
Vic <victor.conil.14@gmail.com>
2018-06-23 15:50:43 +0200

    Clase City

---
+++ Hack4SD/Model/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class City
+    {
+        public int City_id { get; set; }
+        public String City_name { get; set; }
+        public int Zone_id { get; set; }
+        public String Zone_name { get; set; }
+        public int Country_id { get; set; }
+        public String Country_name { get; set; }
+        public Double Latitude { get; set; }
+        public Double Longitude { get; set; }
+    }
+}
