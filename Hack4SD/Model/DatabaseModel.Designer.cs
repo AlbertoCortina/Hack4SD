18f4392e84b981b66536886cfeac4e23efb5440c
Vic <victor.conil.14@gmail.com>
2018-06-23 18:58:52 +0200

    nada

--- Hack4SD/Model/DatabaseModel.Designer.cs
+++ Hack4SD/Model/DatabaseModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Alberto Cortina\Documents\GitHub\Hack4SD2\Hack4SD\Model\DatabaseModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\victo\OneDrive\Documentos\GitHub\Hack4SD2\Hack4SD\Model\DatabaseModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
