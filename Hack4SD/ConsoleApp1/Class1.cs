739b8a2378f4b2a9681f3a05461411037ee1b17d
AlbertoCortina <albertocortina96@gmail.com>
2018-06-24 10:05:16 +0200

    .

--- Hack4SD/ConsoleApp1/Class1.cs
+++ Hack4SD/ConsoleApp1/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using Configuration;
+using Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,29 @@ namespace ConsoleApp1
 {
     class Class1
     {
+        public static IList<User> users = Model.Model.getInstance().users;
+        public static IList<Valoracion> valoraciones = Model.Model.getInstance().valoraciones;
+        public static IList<Campaña> campañas = Model.Model.getInstance().campañas;
+        public static IList<IdeaSostenible> ideasSostenibles = Model.Model.getInstance().ideasSostenibles;
+        public static IList<Experiencia> experiencias = Model.Model.getInstance().experiencias;
 
-       public static void Main()
+        public static void Main()
         {
-            Factory.getService().getValoracionService().getAllValoraciones().ForEach(e => Console.WriteLine(e.Titulo + "\n"));
+            Console.WriteLine("Usuarios");
+            users.ToList().ForEach((u) => Console.WriteLine(u));
+
+            Console.WriteLine("valoraciones");
+            valoraciones.ToList().ForEach((u) => Console.WriteLine(u));
+
+            Console.WriteLine("campañas");
+            campañas.ToList().ForEach((u) => Console.WriteLine(u));
+
+            Console.WriteLine("ideasSostenibles");
+            ideasSostenibles.ToList().ForEach((u) => Console.WriteLine(u));
+
+            Console.WriteLine("experiencias");
+            experiencias.ToList().ForEach((u) => Console.WriteLine(u));
+
             Console.ReadLine();
         }
     }
--- Hack4SD/Model/Model.cs
+++ Hack4SD/Model/Model.cs
@@ -34,47 +34,33 @@ namespace Model
             {
                 new User
                 {
-                    Id = 1,
+                    Id = 0,
                     UserName = "Alberto",
-                    Rol = "Turista",
-                    Valoraciones = valoraciones.Where(v => v.User.Id == 1).ToList(),
-                    Campañas = campañas.Where(v => v.User.Id == 1).ToList(),
-                    IdeasSostenibles = ideasSostenibles.Where(v => v.User.Id == 1).ToList()
+                    Rol = "Turista"
+                },
+                new User
+                {
+                    Id = 1,
+                    UserName = "Victor",
+                    Rol = "Comunidad"
                 },
                 new User
                 {
                     Id = 2,
-                    UserName = "Victor",
-                    Rol = "Comunidad",
-                    Valoraciones = valoraciones.Where(v => v.User.Id == 2).ToList(),
-                    Campañas = campañas.Where(v => v.User.Id == 2).ToList(),
-                    IdeasSostenibles = ideasSostenibles.Where(v => v.User.Id == 2).ToList()
+                    UserName = "Laura",
+                    Rol = "Institucion"
                 },
                 new User
                 {
                     Id = 3,
-                    UserName = "Laura",
-                    Rol = "Institucion",
-                    Valoraciones = valoraciones.Where(v => v.User.Id == 3).ToList(),
-                    Campañas = campañas.Where(v => v.User.Id == 3).ToList(),
-                    IdeasSostenibles = ideasSostenibles.Where(v => v.User.Id == 3).ToList()},
-                new User
-                {
-                    Id = 4,
                     UserName = "Miguel Angel",
-                    Rol = "Comunidad",
-                    Valoraciones = valoraciones.Where(v => v.User.Id == 4).ToList(),
-                    Campañas = campañas.Where(v => v.User.Id == 4).ToList(),
-                    IdeasSostenibles = ideasSostenibles.Where(v => v.User.Id == 4).ToList()
+                    Rol = "Comunidad"
                 },
                 new User
                 {
-                    Id = 5,
+                    Id = 4,
                     UserName = "Daniela",
-                    Rol = "Turista",
-                    Valoraciones = valoraciones.Where(v => v.User.Id == 5).ToList(),
-                    Campañas = campañas.Where(v => v.User.Id == 5).ToList(),
-                    IdeasSostenibles = ideasSostenibles.Where(v => v.User.Id == 5).ToList()
+                    Rol = "Turista"
                 },
             };
 
@@ -83,30 +69,27 @@ namespace Model
             {
                 new Valoracion
                 {
-                    Id = 1,
+                    Id = 0,
                     Titulo = "Increible!",
                     Comentario = "Ha sido una experiencia increible, un sitio precioso, digno de visitar.",
                     Url = "",
-                    Puntuacion = "4",
-                    User = users[0]
-                }
-                , new Valoracion
+                    Puntuacion = "4"
+                },
+                new Valoracion
                 {
-                    Id = 2,
+                    Id = 1,
                     Titulo = "Bonito",
                     Comentario = "Una de las sendas mas bonitas que he podido disfrutar. Además, la zona de descanso esta muy cuidada.",
                     Url = "",
-                    Puntuacion = "5" ,
-                    User = users[1]
-                }
-                , new Valoracion
+                    Puntuacion = "5"
+                },
+                new Valoracion
                 {
-                    Id = 3,
+                    Id = 2,
                     Titulo = "Muy recomendable",
                     Comentario = "Esta catedral en concreto puede ser una de las mejores que he tenido la suerte de visitar. La forma de los arcos es increible",
                     Url= "https://www.youtube.com/watch?v=BUTJQHHzk70",
-                    Puntuacion = "4",
-                    User = users[2]
+                    Puntuacion = "4"
                 }
             };
 
@@ -115,18 +98,16 @@ namespace Model
             {
                 new Campaña
                 {
-                    Id =1,
+                    Id = 0,
                     Titulo="Campaña de limpieza de la senda del oso",
-                    Descripcion = "Últimamente hay mucha basura por todo el recorrido, si quieres visitar la Senda del Oso y además, ayudar a que siga siendo uno de los lugares icónicos de Asturias, apuntate a la campaña de recogida",
-                    User = users[0]
-                }
-                , new Campaña
+                    Descripcion = "Últimamente hay mucha basura por todo el recorrido, si quieres visitar la Senda del Oso y además, ayudar a que siga siendo uno de los lugares icónicos de Asturias, apuntate a la campaña de recogida"
+                },
+                new Campaña
                 {
-                    Id=2,
+                    Id = 1,
                     Titulo = "Visita a la Catedral acompañado de Alguien de la ciudad.",
                     Descripcion = "Miguel se ofrece a enseñarte la Catedral de Oviedo al completo, todo lo que el conoce de este bonito lugar, te será transmitido de primera mano por alguien del lugar." +
-                    "\nIdiomas: Inglés, Alemán, Español.",
-                    User = users[1]
+                    "\nIdiomas: Inglés, Alemán, Español."
                 }
             };
 
@@ -135,18 +116,16 @@ namespace Model
             {
                 new IdeaSostenible
                 {
-                    Id = 1,
+                    Id = 0,
                     Titulo = "Idea sostenible 1",
                     Descripcion = "Descripcion muy bonita de la idea sostenible 1",
-                    Ciudad = "Oviedo",
-                    User = users[0]
+                    Ciudad = "Oviedo"
                 },
                 new IdeaSostenible {
-                    Id = 2,
+                    Id = 1,
                     Titulo = "Idea sostenible 2",
                     Descripcion = "Descripcion muy bonita de la idea sostenible 2",
-                    Ciudad = "Oviedo",
-                    User = users[1]
+                    Ciudad = "Oviedo"
                 }
             };
 
@@ -155,29 +134,60 @@ namespace Model
             {
                 new Experiencia
                 {
-                    Id = 1,
+                    Id = 0,
                     Titulo = "Experiencia 1",
                     Descripcion = "Descripcion 1",
                     Contenido = "Contenido 1",
                     Categoria = "Categoria 1",
                     Ciudad = "Oviedo",
-                    ValoracionMedia = "5",
-                    Valoraciones = valoraciones.Where(v => v.Experiencia.Id == 1).ToList(),
-                    Campañas = campañas.Where(v => v.Experiencia.Id == 1).ToList()
+                    ValoracionMedia = "5"
                 },
                 new Experiencia
                 {
-                    Id = 2,
+                    Id = 1,
                     Titulo = "Experiencia 2",
                     Descripcion = "Descripcion 2",
                     Contenido = "Contenido 2",
                     Categoria = "Categoria 2",
                     Ciudad = "Oviedo",
-                    ValoracionMedia = "5",
-                    Valoraciones = valoraciones.Where(v => v.Experiencia.Id == 1).ToList(),
-                    Campañas = campañas.Where(v => v.Experiencia.Id == 1).ToList()
+                    ValoracionMedia = "5"
                 }
             };
+
+            // Añadir relaciones
+            valoraciones[0].User = users[0];
+            valoraciones[1].User = users[1];
+            valoraciones[2].User = users[2];
+            valoraciones[0].Experiencia = experiencias[0];
+            valoraciones[1].Experiencia = experiencias[1];
+            valoraciones[2].Experiencia = experiencias[0];
+
+            campañas[0].User = users[0];
+            campañas[1].User = users[1];
+            campañas[0].Experiencia = experiencias[0];
+            campañas[1].Experiencia = experiencias[1];
+
+            ideasSostenibles[0].User = users[0];
+            ideasSostenibles[1].User = users[1];
+
+            users[0].Valoraciones = valoraciones.Where(v => v.User.Id == 0).ToList();
+            users[0].Campañas = campañas.Where(v => v.User.Id == 0).ToList();
+            users[0].IdeasSostenibles = ideasSostenibles.Where(v => v.User.Id == 0).ToList();
+            users[1].Valoraciones = valoraciones.Where(v => v.User.Id == 1).ToList();
+            users[1].Campañas = campañas.Where(v => v.User.Id == 1).ToList();
+            users[1].IdeasSostenibles = ideasSostenibles.Where(v => v.User.Id == 1).ToList();
+            users[2].Valoraciones = valoraciones.Where(v => v.User.Id == 2).ToList();
+            users[2].Campañas = campañas.Where(v => v.User.Id == 2).ToList();
+            users[2].IdeasSostenibles = ideasSostenibles.Where(v => v.User.Id == 2).ToList();
+            users[3].Valoraciones = valoraciones.Where(v => v.User.Id == 3).ToList();
+            users[3].Campañas = campañas.Where(v => v.User.Id == 3).ToList();
+            users[3].IdeasSostenibles = ideasSostenibles.Where(v => v.User.Id == 3).ToList();             
+            
+
+            experiencias[0].Valoraciones = valoraciones.Where(v => v.Experiencia.Id == 0).ToList();
+            experiencias[0].Campañas = campañas.Where(v => v.Experiencia.Id == 0).ToList();
+            experiencias[1].Valoraciones = valoraciones.Where(v => v.Experiencia.Id == 1).ToList();
+            experiencias[1].Campañas = campañas.Where(v => v.Experiencia.Id == 1).ToList();
         }
     }
 }
